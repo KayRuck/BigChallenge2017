34333cf92a451331194cc844e36f05005296df6c
Kay Ruck <kay.ruck@yahoo.de>
2017-12-03 19:42:22 +0100

    Silder, Bildvergrößerung etc
    entfert: Lightbox
    hinzugefügt: ganz viel JS

--- doc/js/ownjs.js
+++ doc/js/ownjs.js
@@ -1,18 +1,10 @@
 // Code Snipp made by Siraj from https://bootsnipp.com/snippets/9nVj
 
-("#meinKarusell").carousel();
-    var winWidth = (window).innerWidth();
-    (window).resize(function () {
+("#carousel-example-generic").carousel();
+var winWidth = (window).innerWidth();
+(window).resize(function () {  'use strict';
 
-        if ((window).innerWidth() < winWidth) {
-            ('.carousel-inner>.item>img').css({
-                'min-width': winWidth, 'width': winWidth
-            });
+       
+        if ((window).innerWidth() < winWidth) { ('.carousel-inner>.item>img').css({'min-width': winWidth, 'width': winWidth}); } else {winWidth = (window).innerWidth(); ('.carousel-inner>.item>img').css({'min-width': '', 'width': '' });
         }
-        else {
-            winWidth = (window).innerWidth();
-            ('.carousel-inner>.item>img').css({
-                'min-width': '', 'width': ''
-            });
-        }
-});
+                                });
