7920b6221d7e063291774549613f96098d671ae6
agarzke <alex.garzke@hotmail.de>
2017-12-07 15:41:45 +0100

    Prototype Javascript Tabellen Ein/Ausblenden finished

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,9 +1,40 @@
+var dataopen=false;
+var energyopen=false;
+var anyopen=false;
+
 function datachart() {
-    var datastring = 
-    "<strong>Motorleistung:</strong> 244kW"+
-    "<br/> <strong>Reichweite:</strong> 490 km elektrisch"+
-    "<br/> <strong>Fahrzeuggewicht:</strong> 2.200 kg"+
-    "<br/> <strong>Batterie:</strong> 75 kWh Lithium-lonen"+
-    "<br/> <strong>Beschleunigung 0-100 km/h:</strong> 4,4 Sekunden";
-    document.getElementById("datachart").innerHTML = datastring;
+        if(dataopen==false){
+            if(anyopen==true)
+            {
+                return;
+            }
+            document.getElementById("datachart").style.display="inline";
+            dataopen=true;
+            anyopen=true;
+        }
+        else
+            {
+                document.getElementById("datachart").style.display="none";
+                dataopen=false;
+                anyopen=false;
+            }
+}
+
+
+function energy(){        
+        if(energyopen==false){
+            if(anyopen==true)
+            {
+                return;
+            }
+            document.getElementById("liquid").style.display="inline";
+            energyopen=true;
+            anyopen=true;
+        }
+        else
+            {
+                document.getElementById("liquid").style.display="none";
+                energyopen=false;
+                anyopen=false;
+            }        
 }
