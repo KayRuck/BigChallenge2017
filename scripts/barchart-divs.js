b9f17941388365226fb1362d5354246e3ec7fdf5
JaypsD <33106678+JaypsD@users.noreply.github.com>
2017-12-07 15:23:21 +0100

    bar chart implentiert
    design wird angepasst

--- scripts/barchart-divs.js
+++ scripts/barchart-divs.js
@@ -1,49 +1,12 @@
-(function() {
-    var width = 0, widthScale, barHeight = 30, gutter = 15;
+var data = [4, 8, 15, 16, 23, 42];
 
-    var chart = d3.select(".chart");
-    
-    // data converted from TSV to JS array
-    var data = [
-    {name: 'Locke', value: 100},
-    {name: 'Reyes', value: 50},
-    {name: 'Ford', value: 30},
-    {name: 'Jarrah', value: 75},
-    {name: 'Shephard', value: 23}
-    ];
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
 
-    // coerce to number
-    data.forEach(function(d){
-      d.value = +d.value;
-    });
-
-    var maxVal = d3.max(data, function(d) {
-      return d.value;
-    });
-
-    // sort data
-    data.sort(function(a, b){
-      return Number(b.value) - Number(a.value);
-    });
-
-    var bar = chart.selectAll("div")
+d3.select(".chart")
+  .selectAll("div")
     .data(data)
-    .enter().append("div")
-    .attr('class', 'bar cf')
-    .style("width", function(d) {
-      return Number((d.value/maxVal) * 100) + '%';
-    });
-
-    bar.append("span")
-      .attr("class", 'label')
-      .text(function(d) {
-      return d.value; 
-    });
-
-    bar.append("span")
-      .attr("class", 'name')
-      .text(function(d) {
-      return d.name; 
-    });
-
-})();
+  .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
